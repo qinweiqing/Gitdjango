c279c3e6c4bba8b4c398bcf75c3fc7dcef0d4091
qinweiqing <2536126473@qq.com>
2020-08-29 16:00:11 +0800

    初始提交

--- book_app/urls.py
+++ book_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from book_app import views
 urlpatterns=[
-    path("login/",views.login) #登录功能的路由配置
+    path("login/",views.login), #登录功能的路由配置
+    path("reg/",views.reg)
 ]
--- book_app/views.py
+++ book_app/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def login(request):
     return HttpResponse("login")
+def reg(request):
+    return HttpResponse("reg")
